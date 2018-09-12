d4f7965a5b85d31e4be2765524c1e0a2fea0d6c6
nasawz <nasa127@gmail.com>
2018-09-12 22:28:55 +0800

    投票接口完成

--- src/controllers/vote/main.ts
+++ src/controllers/vote/main.ts
@@ -1,4 +1,5 @@
 import { Parse } from '../../parse';
+import * as _ from 'lodash';
 
 let getActivity: any = activityId => {
   return new Promise(async (resolve, reject) => {
@@ -6,7 +7,7 @@ let getActivity: any = activityId => {
     const a_query = new Parse.Query(Activity);
     a_query.equalTo('objectId', activityId);
     let activity = await a_query.first();
-    resolve(activity.toJSON());
+    resolve(activity);
   });
 };
 
@@ -16,7 +17,7 @@ let getWXUser: any = id => {
     const a_query = new Parse.Query(WXUser);
     a_query.equalTo('objectId', id);
     let wxuser = await a_query.first();
-    resolve(wxuser.toJSON());
+    resolve(wxuser);
   });
 };
 
@@ -25,8 +26,9 @@ let getVoteItem: any = id => {
     const VoteItem = Parse.Object.extend('vote_item');
     const query = new Parse.Query(VoteItem);
     query.equalTo('objectId', id);
+    query.equalTo('status', 0);
     let vote_item = await query.first();
-    resolve(vote_item.toJSON());
+    resolve(vote_item);
   });
 };
 
@@ -35,13 +37,23 @@ let getVoteItemsByUser = user => {
     const VoteItem = Parse.Object.extend('vote_item');
     const query = new Parse.Query(VoteItem);
     query.equalTo('voters', user);
+    query.equalTo('status', 0);
     let vote_items = await query.find();
     resolve(vote_items);
   });
 };
 
 let ckUser = (req, res, cb) => {
+  // !!!!!!!!!!!!!!!!!!!// !!!!!!!!!!!!!!!!!!!// !!!!!!!!!!!!!!!!!!!// !!!!!!!!!!!!!!!!!!!
+  // req.session.user = {
+  //   // objectId: 'vtDCKVLNKh'
+  //   objectId: 'mrt4GgJy8m' // 企业用户
+  // };
+  // req.session.save(function(err) {});
+  // !!!!!!!!!!!!!!!!!!!// !!!!!!!!!!!!!!!!!!!// !!!!!!!!!!!!!!!!!!!// !!!!!!!!!!!!!!!!!!!
+
   let { user } = req.session;
+
   if (user) {
     cb(req, res);
   } else {
@@ -56,19 +68,65 @@ let addVote = async (req, res) => {
     let { user } = req.session;
     let wxuser = await getWXUser(user.objectId);
     let vote_item = await getVoteItem(id);
-    // TODO 规则验证 1 微信用户 2 企业用户
     // 取得当前用户所有的投票项
     let vote_items = await getVoteItemsByUser(wxuser);
 
+    /**
+     * 规则验证
+     * wx_user_rule 微信用户
+     * qywx_user_rule 企业用户
+     */
+    let { wx_user_rule, qywx_user_rule } = activity.toJSON();
+    // 用户类型 1：微信用户 2：企业用户
+    let { type } = wxuser.toJSON();
+
+    if (type == 1) {
+      // 微信用户 wx_user_rule
+      if (wx_user_rule == 0) {
+        // 外部微信用户每个作品都可以投1票
+        let hasVote = false;
+        _.map(vote_items, item => {
+          if (item.toJSON().objectId == id) {
+            hasVote = true;
+          }
+        });
+        if (hasVote) {
+          return res.boom.badRequest('只能为这个作品投1票');
+        }
+      }
+    }
+
+    if (type == 2) {
+      // 微信用户 qywx_user_rule
+      if (qywx_user_rule == 0) {
+        // 微信用户每组可投3票,每个作品限1票
+        let hasVote = false;
+        let group_len = 0;
+        _.map(vote_items, item => {
+          if (item.toJSON().objectId == id) {
+            hasVote = true;
+          }
+          if (item.toJSON().category == vote_item.toJSON().category) {
+            group_len++;
+          }
+        });
+        if (group_len >= 3) {
+          return res.boom.badRequest('本组最多能投3票');
+        }
+        if (hasVote) {
+          return res.boom.badRequest('只能为这个作品投1票');
+        }
+      }
+    }
     vote_item.increment('score');
     let relation = vote_item.relation('voters');
     relation.add(wxuser);
     vote_item.save().then(
-      res => {
-        return res.json(res);
+      data => {
+        return res.json(data);
       },
       err => {
-        return res.badRequest('add vote fail', err);
+        return res.boom.badRequest('add vote fail', err);
       }
     );
   });
@@ -80,19 +138,50 @@ let addVoteItem = async (req, res) => {
     let activity = await getActivity(activityId);
     let { user } = req.session;
     let wxuser = await getWXUser(user.objectId);
-    // TODO 资格检测 1 微信用户 2 企业用户 0 任意用户
-    // TODO 已提交检测
+
+    let { join_rule } = activity.toJSON();
+    // 用户类型 1：微信用户 2：企业用户
+    let { type } = wxuser.toJSON();
+    /**
+     * 资格检测 join_rule
+     * 2 : 企业用户可参与
+     * 1 : 全部用户可参与
+     */
+    if (join_rule == 2) {
+      if (type != 2) {
+        return res.boom.badRequest('只有企业用户才可参与活动');
+      }
+    }
+    // 已提交检测
     const VoteItem = Parse.Object.extend('vote_item');
+    const query = new Parse.Query(VoteItem);
+    query.equalTo('activity', activity);
+    query.equalTo('owner', wxuser);
+    query.equalTo('status', 0);
+    if (await query.first()) {
+      return res.boom.badRequest('您已参与活动了');
+    }
     let data = req.body;
-    const voteItem = new VoteItem(data);
+    let u = wxuser.toJSON();
+    const voteItem = new VoteItem({
+      title: data.title,
+      category: data.category,
+      desc: data.desc,
+      pic: data.pic,
+      score: 0,
+      status: 0,
+      name: u.name,
+      mobile: u.mobile,
+      userid: u.userid
+    });
     voteItem.set('activity', activity);
     voteItem.set('owner', wxuser);
     voteItem.save().then(
-      res => {
-        return res.json(res);
+      result => {
+        return res.json(result);
       },
       err => {
-        return res.badRequest('save vote_item fail', err);
+        return res.boom.badRequest('save vote_item fail', err);
       }
     );
   });
@@ -111,7 +200,7 @@ let delVoteItem = async (req, res) => {
     query.equalTo('objectId', id);
     let vote_item = await query.first();
     if (!vote_item) {
-      return res.badRequest('del vote_item fail');
+      return res.boom.badRequest('del vote_item fail');
     }
     vote_item.set('status', -1);
     vote_item.save().then(
@@ -119,7 +208,7 @@ let delVoteItem = async (req, res) => {
         return res.json(res);
       },
       err => {
-        return res.badRequest('del vote_item fail', err);
+        return res.boom.badRequest('del vote_item fail', err);
       }
     );
   });
@@ -135,6 +224,7 @@ let myVoteItem = async (req, res) => {
     const query = new Parse.Query(VoteItem);
     query.equalTo('activity', activity);
     query.equalTo('owner', wxuser);
+    query.equalTo('status', 0);
     let vote_item = await query.first();
     if (vote_item) {
       return res.json(vote_item);
--- src/controllers/vote/route.ts
+++ src/controllers/vote/route.ts
@@ -2,7 +2,7 @@ import { addVote, addVoteItem, delVoteItem, myVoteItem } from './main';
 
 export default (base, app) => {
   app.post(`${base}/addVote/:activityId/:id`, addVote);
-  app.post(`${base}/addVoteItem/:activityId/:id`, addVoteItem);
-  app.get(`${base}/myVoteItem/:activityId/`, myVoteItem);
+  app.post(`${base}/addVoteItem/:activityId`, addVoteItem);
+  app.get(`${base}/myVoteItem/:activityId`, myVoteItem);
   app.delete(`${base}/delVoteItem/:activityId/:id`, delVoteItem);
 };
